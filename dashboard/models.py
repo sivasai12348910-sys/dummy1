2e3ae1412764a6b4e77be1822b6d328cf1778c16
sivasai12348910-sys <sivasai12348910@gmail.com>
2026-03-22 16:01:34 +0000

    image

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class SensorData(models.Model):
+    device_id = models.CharField(max_length=50)
+
     temperature = models.FloatField()
     humidity = models.FloatField()
     soil_moisture = models.IntegerField()
     ph = models.FloatField()
-    image = models.ImageField(upload_to='sensor_images/', null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return str(self.created_at)
+    image = models.ImageField(upload_to='sensor_images/', null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,25 +3,24 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .models import SensorData
+from django.core.files.base import ContentFile
 
 @csrf_exempt
 def sensor_data_api(request):
 
     if request.method == "POST":
         try:
-            temperature = request.POST.get("temperature")
-            humidity = request.POST.get("humidity")
-            soil = request.POST.get("soil_moisture")
-            ph = request.POST.get("ph")
+            data = json.loads(request.body)
 
             SensorData.objects.create(
-                temperature=temperature,
-                humidity=humidity,
-                soil_moisture=soil,
-                ph=ph,
+                device_id=data.get("device_id"),
+                temperature=data.get("temperature"),
+                humidity=data.get("humidity"),
+                soil_moisture=data.get("soil_moisture"),
+                ph=data.get("ph"),
             )
 
-            return JsonResponse({"status": "stored"})
+            return JsonResponse({"status": "sensor stored"})
 
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=400)
@@ -30,13 +29,31 @@ def sensor_data_api(request):
 
 @csrf_exempt
 def upload_image(request):
+
     if request.method == "POST":
-        image_data = request.body
+        try:
+            device_id = request.GET.get("device_id")
 
-        with open("media/latest.jpg", "wb") as f:
-            f.write(image_data)
+            if not device_id:
+                return JsonResponse({"error": "device_id missing"}, status=400)
 
-        return JsonResponse({"status": "image saved"})
+            obj = SensorData.objects.filter(device_id=device_id).last()
+
+            if not obj:
+                return JsonResponse({"error": "No matching sensor data"}, status=404)
+
+            image_file = ContentFile(request.body, name=f"{device_id}.jpg")
+
+            obj.image.save(f"{device_id}.jpg", image_file)
+            obj.save()
+
+            return JsonResponse({"status": "image saved"})
+
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+
+    return JsonResponse({"error": "POST only"}, status=405)
+
 def dashboard_page(request):
     data = SensorData.objects.order_by("-created_at")
     return render(request, "dashboard.html", {"data": data})
